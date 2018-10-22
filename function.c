3871169c89a349fdf3716dc18f03df0f29298b90
auocoder <ausekaruo17.comp@coep.ac.in>
2018-10-22 17:52:09 +0530

    modifications to fread and fwrite, ftell and fseek added

--- function.c
+++ function.c
@@ -127,23 +127,28 @@ unsigned long Fread(void *ptr, unsigned long size, unsigned long nmem, File *fp)
 	long toberead = size * nmem;
 	int r = 0;
 	void *tr;
+	/*If size of buffer is less than amount to be read , it is better to call read
+	directly rather than call buffill() n number of times, because each buffill() will call read once*/
 	if(toberead > BUFSIZE){
-		//dosomething maybe unbuffer
+		
+		fp -> bufornot = 'u';
+		//youre forgetting something else
 	}
 	fp -> last = 'r';
-	if(fp == &ptrarr[0]){
-		fp -> bufornot = 'u';
-	}
+	/*if(fp == &ptrarr[0]){
+		fp -> bufornot = 'u';       check if this works
+	}*/
 	if(fp -> left == 0)
 		 r = buffill(fp);
 	if(/*r == INT_MIN || */fp -> flag == EOF)
 		return 0;
-	if(fp -> bufornot == 'u'){
-		/*then read and write unbuffered;*/
+	if(fp -> bufornot == 'u'){        /*then read and write unbuffered; What if you already have something in buffer?*/
+		
 		int rd = read(fp -> fd, ptr, toberead);
 		if(rd < toberead){
 			fp -> flag = EOF;
 		}
+		fp -> bufornot = 'b';
 		return rd / size;
 	}
 	if(fp -> left < toberead){
@@ -166,7 +171,15 @@ unsigned long Fwrite(void *ptr, unsigned long size, unsigned long nmem, File *fp
 	long towrite = size * nmem;
 	int i = 0;
 	fp -> last = 'w';
-	if(fp == &(ptrarr[2]) || fp == &(ptrarr[1])){
+	/*If size of buffer is less than amount to be written , it is better to call write 
+	directly rather than call bufflush() n number of times, because each bufflush() will call write once*/
+	if(towrite > BUFSIZE){  
+		//flush buffer ,call fseek, pos 0, seekcur 
+		fp -> bufornot = 'u';
+		// now you have to set bufornot to \0 again after you finish write;
+		//done after write check if works
+	}
+	if(fp == &(ptrarr[2])){
 		fp -> bufornot = 'u';
 	}
 	if(fp -> buf == NULL || fp -> left == 0){
@@ -180,6 +193,7 @@ unsigned long Fwrite(void *ptr, unsigned long size, unsigned long nmem, File *fp
 			/*this is write error*/
                 	fp -> flag = EOF;
 		}
+		fp -> bufornot = 'b';//check this
 		return wr / size;
 	}
 	if(towrite > fp -> left)
@@ -206,3 +220,85 @@ int Fclose(File *fp){
 		return -1;
 	return 1;
 }
+
+long Ftell(File *fp){//havent dealt with all modes yet
+	long currpos;
+	if(fp -> last == '\0')
+		return 0; 
+	currpos = lseek(fp -> fd, 0, SEEK_CUR);
+	if(currpos == -1)
+		return -1;
+	if(fp -> flag == R || fp -> last == 'r'){
+		return (currpos - fp -> left);
+	}
+	else if(fp -> flag == W || fp -> flag == A || fp -> last == 'w'){
+		printf("in ftell this is fp -> left %d", fp -> left);
+		printf("in ftell retval of ftell is %ld\n", currpos + BUFSIZE - fp -> left); 
+		return (currpos + BUFSIZE - fp -> left);
+	}
+	
+}
+
+int Fseek(File *fp, long pos, int whence){
+	if(whence != SEEK_CUR && whence != SEEK_SET && whence != SEEK_END){
+		errno = EINVAL;
+		return 1;
+	}
+	if(whence == SEEK_SET && pos < 0){
+		errno = EINVAL;
+		return 1;
+	}
+	if(whence == SEEK_CUR){
+		if(fp -> last == 'r' && pos > 0 && pos < fp -> left){
+			fp -> next = fp -> next + pos;
+			fp -> left = fp -> left - pos;
+			return 0;
+			
+		}
+		if(fp -> last == 'r' && pos < 0 &&  -(pos) < BUFSIZE - fp -> left){
+			fp -> next = fp -> next + pos;
+			fp -> left = fp -> left - pos;
+			return 0;
+		}
+		if(fp -> last == 'w' && pos < 0 && -(pos) < BUFSIZE - fp -> left){
+			printf("NOT using memset here\n");
+			fp -> next = fp -> next + pos;
+			fp -> left = fp -> left - pos ;
+			return 0;
+		}
+		if(fp -> last == 'w' && pos > 0 && pos < fp -> left){//problems here? use mycat and see entire file is not read, but gedit shows entire file. but when i was not attempting to move ptr in buffer, things work fine
+			printf("fp -> left = %d\n", fp -> left);
+			printf("About to use memset\n");
+			memset(fp -> next, '\0', pos);// * sizeof(pos));
+			fp -> next = fp -> next + pos;
+			fp -> left = fp -> left - pos;
+			printf("fp -> left after = %d\n", fp -> left);
+			return 0;
+		}
+	}
+	//what if user tries to seek beyond buffer?
+	//you should fill buffer
+	switch(fp -> flag){
+		case R:
+			lseek(fp -> fd, -(fp -> left), SEEK_CUR);
+			break;
+		case W:
+		case A:
+			bufflush(fp);
+			break;
+		case AP:
+		case WP:
+		case RP:
+			if(fp -> last == 'w')
+				bufflush(fp);
+			else if(fp -> last == 'r')
+				lseek(fp -> fd, -(fp -> left), SEEK_CUR);
+			break;
+		default:
+			return 1;
+			break;
+	}
+	lseek(fp -> fd, pos, whence);
+	return 0;
+}
+
