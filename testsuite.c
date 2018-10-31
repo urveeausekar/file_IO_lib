87f9d965b3d286443cd1b7f88fdddd7a2700a710
auocoder <ausekaruo17.comp@coep.ac.in>
2018-10-31 09:52:08 +0530

    tests for fwrite added, tests for fread modified a little

--- testsuite.c
+++ testsuite.c
@@ -4,17 +4,24 @@
 #include<errno.h>
 
 int testFopen();
+/* for testing the "r" mode */
 int testFread();
+/* for testing the "w" and "a" mode */
 int testFwrite();
+/*for testing file positioning systems*/
 int testFseek();
 int testFtell();
 int testFgetpos();
 int testFsetpos();
+/*for testing the + modes ie "r+", "w+" and "a+"*/
+int testPlusmodes();
 int testFeof();
+/*above functions will return 1 on success and 0 on failure*/
 
 int main(int argc, char *argv[]){
 	int r = testFopen();
 	testFread();
+	testFwrite();
 	return 0;
 }
 
@@ -79,7 +86,7 @@ int testFopen(){
 		printf("Failure: return value not NULL or errno not set correctly\n");
 		error = 1;
 	}
-	
+	printf("\n\n");
 	
 	if(error != 1)
 		return 1;
@@ -103,42 +110,46 @@ int testFread(){
 	fs = fopen("test1", "r");
 	if(f == NULL || fs == NULL){
 		perror("cant open file\n");
-		return 0;
-	}
-	ret1 = Fread(str1, 1, 1000, f);
-	ret2 = fread(str2, 1, 1000, fs);
-	printf("ret1 = %lu ret2 = %lu\n", ret1, ret2);
-	str1[ret1] = '\0';
-	str2[ret2] = '\0';
-	Fclose(f);
-	fclose(fs);
-	if(strcmp(str1, str2) == 0)
-		printf("Success\n");
-	else{
-		printf("Failure: some error occured\n");
+		//write which error in some file?
 		error = 1;
-		return 0;
 	}
-	
+	else{
+		ret1 = Fread(str1, 1, 1000, f);
+		ret2 = fread(str2, 1, 1000, fs);
+		printf("ret1 = %lu ret2 = %lu\n", ret1, ret2);
+		str1[ret1] = '\0';
+		str2[ret2] = '\0';
+		Fclose(f);
+		fclose(fs);
+		if(strcmp(str1, str2) == 0)
+			printf("Success\n");
+		else{
+			printf("Failure: some error occured\n");
+			error = 1;
+			return 0;
+		}
+	}
+		
 	printf("case:text file larger than buffer size\n");
 	f = Fopen("test2", "r");
 	fs = fopen("test2", "r");
 	if(f == NULL || fs == NULL){
 		perror("cant open file\n");
-		return 0;
 	}
-	ret1 = Fread(str1, 1, 1508, f);
-	ret2 = fread(str2, 1, 1508, fs);
-	str1[ret1] = '\0';
-	str2[ret2] = '\0';
-	Fclose(f);
-	fclose(fs);
-	if(strcmp(str1, str2) == 0)
-		printf("Success\n");
 	else{
-		printf("Failure: some error occured\n");
-		error = 1;
-		return 0;
+		ret1 = Fread(str1, 1, 1508, f);
+		ret2 = fread(str2, 1, 1508, fs);
+		str1[ret1] = '\0';
+		str2[ret2] = '\0';
+		Fclose(f);
+		fclose(fs);
+		if(strcmp(str1, str2) == 0)
+			printf("Success\n");
+		else{
+			printf("Failure: some error occured\n");
+			error = 1;
+			return 0;
+		}
 	}
 	
 	printf("case:binary file smaller than buffer size\n");
@@ -148,23 +159,25 @@ int testFread(){
 		perror("cant open file\n");
 		return 0;
 	}
-	while(i < 15){
-		Fread(&i, sizeof(int), 1, f);
-		fread(&j, sizeof(int), 1, fs);
-		if(i != j){
-			error = 1;
-			break;
+	else{
+		while(i < 15){
+			Fread(&i, sizeof(int), 1, f);
+			fread(&j, sizeof(int), 1, fs);
+			if(i != j){
+				error = 1;
+				break;
+			}
 		}
+		if(error == 1){
+			printf("Failure\n");
+			error = 0;
+		}
+		else
+			printf("Success\n");
+			
+		Fclose(f);
+		fclose(fs);
 	}
-	if(error == 1){
-		printf("Failure\n");
-		error = 0;
-	}
-	else
-		printf("Success\n");
-		
-	Fclose(f);
-	fclose(fs);
 	
 	printf("case:binary file larger than buffer size\n");
 	j = 0;
@@ -174,26 +187,122 @@ int testFread(){
 		perror("cant open file\n");
 		return 0;
 	}
-	while(k < 260){
-		Fread(&i, sizeof(int), 1, f);
-		fread(&j, sizeof(int), 1, fs);
+	else{
+		while(k < 260){
+			Fread(&i, sizeof(int), 1, f);
+			fread(&j, sizeof(int), 1, fs);
+			if(i != j){
+				error = 1;
+				break;
+			}
+			k++;
+		}
+		
+		if(error == 1){
+			printf("Failure\n");
+			error = 0;
+		}
+		else
+			printf("Success\n");
+			
+		Fclose(f);
+		fclose(fs);
+		printf("\n\n");
+	}
+	
+	return 1;
+}
+
+int testFwrite(){
+	File *fr, *fw, *f;
+	char str1[64] = "Richard Phillips Feynman", str2[64], str3[64] = "Richard Phillips Feynman was a great physicist!";
+	int error = 9, i = 0, j = 0;
+	
+	printf("TESTING Fwrite\ntesting \"w\" mode\n");
+	fw = Fopen("writetest1", "w");
+	if(fw == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	Fwrite(str1, 1, strlen(str1), fw);
+	Fclose(fw);
+	/*testing Fwrite for writing charachters*/
+	printf("testing with text file\n");
+	fr = Fopen("writetest1", "r");
+	if(fw == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	printf("%ld\n", strlen(str1));
+	Fread(str2, 1, 24, fr);
+	str2[24] = '\0';
+	//printf("%s\n", str2);
+	if(strcmp(str1, str2) == 0){
+		printf("Success\n");
+	}
+	else{
+		printf("Failure\n");
+		error = 1;
+	}
+	
+	/*testing Fwrite for writing non charachters: here integers*/
+	Fclose(fr);
+	printf("testing with binary file of size greater than buffer\n");
+	fw = Fopen("writetest2", "w");
+	if(fw == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	while(i < 300){
+		Fwrite(&i, sizeof(int), 1, fw);
+		i++;
+	}
+	
+	Fclose(fw);
+	fr = Fopen("writetest2", "r");
+	if(fr == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	i = 0;
+	error = 0;
+	while(i < 300){
+		Fread(&j, sizeof(int), 1, fr);
 		if(i != j){
 			error = 1;
 			break;
 		}
-		k++;
+		i++;
 	}
-	
 	if(error == 1){
 		printf("Failure\n");
 		error = 0;
 	}
 	else
 		printf("Success\n");
-		
+	Fclose(fr);
+	
+	/*Now testing append mode*/
+	printf("Now testing append mode \"a\"\n");
+	strcpy(str1, " was a great physicist!");
+	f = Fopen("writetest1", "a");
+	if(f == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	Fwrite(str1, 1, 23, f);
 	Fclose(f);
-	fclose(fs);
-	
-	
-	return 1;
+	f = Fopen("writetest1", "r");
+	if(f == NULL){
+		perror("cant open file\n");
+		return 0;
+	}
+	Fread(str2, 1, 47, f);
+	str2[47] = '\0';
+	if(strcmp(str2, str3) == 0){
+		printf("Success\n");
+	}
+	else
+		printf("Failure!\n");
+	return error;
 }
